4dbc7cb2dddc5b3c8620b81cc5bc878a3b44fc13
Juuso Kokkonen <juusokokkonen98@gmail.com>
2021-02-18 04:22:35 +0200

    added add, delete and edit functionalities for booklist

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,7 +1,13 @@
 package com.example.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +16,18 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(BookRepository sRepository) {
+		return (args) -> {
+			Book demo1 = new Book("Testi", "Testiauthor", "2021", "testi", (long) 20);
+			Book demo2 = new Book("Demo", "Demo-author", "2010", "ISBN", (long) 19);
+			Book demo3 = new Book(":)", "Testiauthor", "2021", "testi", (long) 20);
+			
+			sRepository.save(demo1);
+			sRepository.save(demo2);
+			sRepository.save(demo3);
+		};
+	}
+	
 }
+
--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,14 +1,68 @@
 package com.example.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 
+	@Autowired
+	private BookRepository bRepository;
+	
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String mainPage() {
 		return "index";
 	}
+	
+	// Booklist page
+	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+	public String bookList(Model model) {
+		model.addAttribute("books", bRepository.findAll());
+		return "booklist";
+	}
+	
+	// Delete a book
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		bRepository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	
+	// Add a book
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	// Save a book
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveBook(Book book) {
+		bRepository.save(book);
+		return "redirect:booklist";
+	}
+	
+	// Edit a book
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("Book", bRepository.findById(bookId));
+		return "editbook";
+	}
+	
+	// Save edited book
+	@RequestMapping(value = "/editsave/{id}", method = RequestMethod.POST)
+	public String saveEdited(@PathVariable("id") Long bookId, Book book) {
+		Book newBook = book;
+		bRepository.deleteById(bookId);
+		bRepository.save(newBook);
+		return "redirect:../booklist";
+	}
+	
 }
